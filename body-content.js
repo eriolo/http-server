ebc3a39baf25e0cfcf79a3e5a7a43877f596b5fb
erik-olofsson2 <erik.olofsson2@schibsted.se>
2019-01-03 12:12:30 +0100

    Updates

---
+++ body-content.js
@@ -0,0 +1,30 @@
+const { createServer } = require('http');
+
+const PORT = process.env.PORT || 8080;
+// Test with for instance curl -X POST -d '{"property": true}' http://localhost:8080
+createServer((req, res) => {
+    if (req.method === 'POST') {
+        let data = '';
+
+        req.on('data', chunk => {
+            data += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                const requestData = JSON.parse(data);
+                requestData.ourMessage = 'success';
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify(requestData));
+            } catch (err) {
+                res.statusCode = 400;
+                res.end('Invalid request');
+            }
+        });
+    } else {
+        res.statusCode = 400;
+        res.end('Unsupported method - POST JSON');
+    }
+}).listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
---
+++ router.js
@@ -0,0 +1,47 @@
+const { createServer } = require('http');
+const querystring = require('querystring')
+const url = require('url');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 8080;
+
+// Simple web server
+// https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework
+createServer((req, res) => {
+    const URL = req.url;
+    const { query } = url.parse(URL, true);
+    const queryString = querystring.stringify(query);
+    const urlWithoutQuery = URL.replace(queryString, '').replace(/\?/, '');
+    const pathname = replaceSlash(urlWithoutQuery);
+
+    // Redirect url with trailing slash and no queries
+    // to the url without trailing slash
+    if (/\/$/.test(urlWithoutQuery) && urlWithoutQuery !== '/') {
+        res.writeHead(301, { 'Location': pathname });
+        return res.end();
+    }
+
+    let filePath = `./pages${pathname}/index.html`;
+
+    // Search for url on file system
+    fs.readFile(filePath, (err, content) => {
+        // Error 'no such file or directory'
+        if (err && err.code === 'ENOENT') {
+            fs.readFile('./pages/404/index.html', (err, content) => {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content, 'utf-8');
+            });
+        } else if (err) {
+            res.writeHead(500);
+            res.end(`Error code ${error.code}`);
+            res.end();
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf-8');
+        }
+    });
+}).listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+const replaceSlash = url => url.replace(/\/$/, '');
