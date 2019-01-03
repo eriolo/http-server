3cbcd6cd5877bc56556666fd0f31124f37a8578d
erik-olofsson2 <erik.olofsson2@schibsted.se>
2019-01-03 16:54:42 +0100

    Add prettier and eslint

--- body-content.js
+++ body-content.js
@@ -3,28 +3,28 @@ const { createServer } = require('http');
 const PORT = process.env.PORT || 8080;
 // Test with for instance curl -X POST -d '{"property": true}' http://localhost:8080
 createServer((req, res) => {
-    if (req.method === 'POST') {
-        let data = '';
+  if (req.method === 'POST') {
+    let data = '';
 
-        req.on('data', chunk => {
-            data += chunk;
-        });
+    req.on('data', chunk => {
+      data += chunk;
+    });
 
-        req.on('end', () => {
-            try {
-                const requestData = JSON.parse(data);
-                requestData.ourMessage = 'success';
-                res.setHeader('Content-Type', 'application/json');
-                res.end(JSON.stringify(requestData));
-            } catch (err) {
-                res.statusCode = 400;
-                res.end('Invalid request');
-            }
-        });
-    } else {
+    req.on('end', () => {
+      try {
+        const requestData = JSON.parse(data);
+        requestData.ourMessage = 'success';
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(requestData));
+      } catch (err) {
         res.statusCode = 400;
-        res.end('Unsupported method - POST JSON');
-    }
+        res.end('Invalid request');
+      }
+    });
+  } else {
+    res.statusCode = 400;
+    res.end('Unsupported method - POST JSON');
+  }
 }).listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`); // eslint-disable-line no-console
 });
--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const { createServer } = require('http');
-const querystring = require('querystring')
+const querystring = require('querystring');
 const url = require('url');
 const fs = require('fs');
 
@@ -8,40 +8,40 @@ const PORT = process.env.PORT || 8080;
 // Simple web server
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Node_server_without_framework
 createServer((req, res) => {
-    const URL = req.url;
-    const { query } = url.parse(URL, true);
-    const queryString = querystring.stringify(query);
-    const urlWithoutQuery = URL.replace(queryString, '').replace(/\?/, '');
-    const pathname = replaceSlash(urlWithoutQuery);
+  const URL = req.url;
+  const { query } = url.parse(URL, true);
+  const queryString = querystring.stringify(query);
+  const urlWithoutQuery = URL.replace(queryString, '').replace(/\?/, '');
+  const pathname = replaceSlash(urlWithoutQuery);
 
-    // Redirect url with trailing slash and no queries
-    // to the url without trailing slash
-    if (/\/$/.test(urlWithoutQuery) && urlWithoutQuery !== '/') {
-        res.writeHead(301, { 'Location': pathname });
-        return res.end();
+  // Redirect url with trailing slash and no queries
+  // to the url without trailing slash
+  if (/\/$/.test(urlWithoutQuery) && urlWithoutQuery !== '/') {
+    res.writeHead(301, { Location: pathname });
+    return res.end();
+  }
+
+  let filePath = `./pages${pathname}/index.html`;
+
+  // Search for url on file system
+  fs.readFile(filePath, (err, content) => {
+    // Error 'no such file or directory'
+    if (err && err.code === 'ENOENT') {
+      fs.readFile('./pages/404/index.html', (err, content) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content, 'utf-8');
+      });
+    } else if (err) {
+      res.writeHead(500);
+      res.end(`Error code ${err.code}`);
+      res.end();
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
     }
-
-    let filePath = `./pages${pathname}/index.html`;
-
-    // Search for url on file system
-    fs.readFile(filePath, (err, content) => {
-        // Error 'no such file or directory'
-        if (err && err.code === 'ENOENT') {
-            fs.readFile('./pages/404/index.html', (err, content) => {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(content, 'utf-8');
-            });
-        } else if (err) {
-            res.writeHead(500);
-            res.end(`Error code ${error.code}`);
-            res.end();
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content, 'utf-8');
-        }
-    });
+  });
 }).listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`); // eslint-disable-line no-console
 });
 
 const replaceSlash = url => url.replace(/\/$/, '');
